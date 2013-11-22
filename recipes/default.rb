68d7776de5dd1dce38e24e39bcfdd89af7799c5b
Kaazing Technical Operations team <technical.operations@kaazing.com>
2013-11-22 11:27:57 -0800

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,3 +34,16 @@ fog.run_action( :install )
 
 require 'rubygems'
 Gem.clear_paths
+
+route53_record "create a record" do
+  name  "test"
+  value "16.8.4.2"
+  type  "A"
+
+  zone_id               node[:route53][:zone_id]
+  aws_access_key_id     node[:route53][:aws_access_key_id]
+  aws_secret_access_key node[:route53][:aws_secret_access_key]
+
+  action :create
+end
+
